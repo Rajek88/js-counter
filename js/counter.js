583f0b9509fce9dc1573def0630396602e6ceeb6
Rajendra Kulkarni <frankly.coder@gmail.com>
2021-07-28 15:30:41 +0530

    Upgraded color scheme and added beep

--- js/counter.js
+++ js/counter.js
@@ -1,5 +1,6 @@
 // console.log(document.querySelector('.neu-input'));
 
+var audio = new Audio('https://media.geeksforgeeks.org/wp-content/uploads/20190531135120/beep.mp3');
 function counter(btn){
     var countTill = document.querySelector('.neu-input').value;
     let count = parseInt(countTill);
@@ -14,6 +15,9 @@ function counter(btn){
     startBtn.className = 'start-btn-clicked';
     var interval = setInterval(function(){
         if(count == 0){
+            // document.querySelector('.counter').style.backgroundColor = 'tomato';
+            // document.querySelector('.counter').style.color='white';
+            audio.play();
             window.alert("Time Up");
             clearInterval(interval);
             btn.disabled = false;
