a42a026eb1209f5a773954bfac55e6aa6db00ec3
Rajendra Kulkarni <frankly.coder@gmail.com>
2021-07-28 09:34:13 +0530

    Updated the btn and added more btn features

--- js/counter.js
+++ js/counter.js
@@ -1,6 +1,6 @@
 // console.log(document.querySelector('.neu-input'));
 
-function counter(){
+function counter(btn){
     var countTill = document.querySelector('.neu-input').value;
     let count = parseInt(countTill);
     console.log(count);
@@ -9,10 +9,15 @@ function counter(){
         return;
     }
     document.querySelector('#current').innerHTML = count;
+    btn.disabled = true;
+    var startBtn = document.getElementById('startBtn');
+    startBtn.className = 'start-btn-clicked';
     var interval = setInterval(function(){
         if(count == 0){
             window.alert("Time Up");
             clearInterval(interval);
+            btn.disabled = false;
+            startBtn.className = 'start-btn';
             return;
         }
         count--;
